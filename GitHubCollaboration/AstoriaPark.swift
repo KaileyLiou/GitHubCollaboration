4ebfe72a43d37f12ac1c57dff0125681babaecfa
Naqaa E <elzoeirynaqaa@gmail.com>
2025-08-12 11:55:58 -0400

    astoria park stars

--- GitHubCollaboration/AstoriaPark.swift
+++ GitHubCollaboration/AstoriaPark.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 struct AstoriaPark: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Astoria Park")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                Spacer()
+                Text("★★★★")
+                    .font(.system(size:30))
+                    .foregroundColor(Color.yellow)
+            } .padding()
+        }
+        Spacer()
     }
-}
-
+    }
 #Preview {
     AstoriaPark()
 }
