11220254f50dc34c52b1dabecf6c8c6caeaa3055
Maddy <m.s.woo2008@gmail.com>
2025-08-12 11:47:05 -0400

    maddy change - west side highway title + stars + notes

--- GitHubCollaboration/West Side Highway.swift
+++ GitHubCollaboration/West Side Highway.swift
@@ -9,9 +9,37 @@ import SwiftUI
 
 struct West_Side_Highway: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        VStack (alignment: .leading){
+            HStack {
+               
+                Text("West Side Highway")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.leading)
+                Spacer() //between texts
+                Text ("★★★★")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.yellow)
+
+                Spacer()//pushes left
+                
+            }//end of HStack
+            
+            Spacer()//pushes up //WSH up
+                .frame(height:50)
+            Text ("Notes")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+            
+        }//end of VStack
+        Spacer ()
+    
+    } //end of body
+    
+}//end of struct
+
+
 
 #Preview {
     West_Side_Highway()
