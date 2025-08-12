c1378403e37960ee3fd45f7977338d60d34171b1
SonyaDC7 <sonyacisse7@gmail.com>
2025-08-12 11:25:00 -0400

    put the navigation links in a nav stack and added central park back to the mix

--- GitHubCollaboration/ContentView.swift
+++ GitHubCollaboration/ContentView.swift
@@ -9,33 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            NavigationLink(destination: CentralPark()) {
-                Text("Central Park")
-                    .font(.headline)
-                    .foregroundColor(Color.black)
-            }//end central park nav link
-            NavigationLink(destination: AstoriaPark()) {
-                Text("Astoria Park")
-                    .font(.headline)
-                    .foregroundColor(Color.black)
-            } //end astoria park nav link
-            NavigationLink(destination: West_Side_Highway()) {
-                Text("West Side Highway")
-                    .font(.headline)
-                    .foregroundColor(Color.black)
-            } //end west side highway nav link
-            NavigationLink(destination: Seaport()) {
-                Text("Seaport")
-                    .font(.headline)
-                    .foregroundColor(Color.black)
-            } //end seaport nav link
-        }
-        .padding()
+        NavigationStack { //start nav stack
+            
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+                NavigationLink(destination: CentralPark()) {
+                    Text("Central Park")
+                        .font(.headline)
+                        .foregroundColor(Color.black)
+                }//end central park nav link
+                NavigationLink(destination: AstoriaPark()) {
+                    Text("Astoria Park")
+                        .font(.headline)
+                        .foregroundColor(Color.black)
+                } //end astoria park nav link
+                NavigationLink(destination: West_Side_Highway()) {
+                    Text("West Side Highway")
+                        .font(.headline)
+                        .foregroundColor(Color.black)
+                } //end west side highway nav link
+                NavigationLink(destination: Seaport()) {
+                    Text("Seaport")
+                        .font(.headline)
+                        .foregroundColor(Color.black)
+                } //end seaport nav link
+            }//end vstack
+            .padding()
+        } //end nav stack
     }
 }
 
