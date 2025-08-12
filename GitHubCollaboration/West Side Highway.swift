cb38edfe90c51d9748c8fa98ec250df39b8f5b32
SonyaDC7 <sonyacisse7@gmail.com>
2025-08-12 09:54:27 -0400

    I created new SwiftUI files from the template for each route and inserted a nav link on the home page for each route

---
+++ GitHubCollaboration/West Side Highway.swift
@@ -0,0 +1,18 @@
+//
+//  West Side Highway.swift
+//  GitHubCollaboration
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+struct West_Side_Highway: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    West_Side_Highway()
+}
